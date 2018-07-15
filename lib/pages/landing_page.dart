31e241d46b34c6460ff7e0e0a01d7199ff41ab04
Shashwath Kamath <kamathshashwath@Shashwaths-MacBook-Pro.local>
2018-07-15 18:34:58 -0400

    Initial commit

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Material(
+      color: Colors.greenAccent,
+    );
+  }
+}
