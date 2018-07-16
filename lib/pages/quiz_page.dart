27cd96e42492f13b22970fc11952bb8bd2ea21cf
Shashwath Kamath <kamathshashwath1991@gmail.com>
2018-07-16 09:33:22 -0400

    New Questions and answer widgets comitted.

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -2,52 +2,46 @@ import 'package:flutter/material.dart';
 import '../utils/questions.dart';
 import '../utils/quiz.dart';
 
-class QuizPage extends StatefulWidget{
-
+class QuizPage extends StatefulWidget {
   @override
   State createState() => new QuizPageState();
 }
 
-class QuizPageState extends State<QuizPage>{
-
+class QuizPageState extends State<QuizPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Stack(
       children: <Widget>[
         new Column(
           children: <Widget>[
             new Expanded(
-                  child: new Material(
-                  color: Colors.greenAccent,
-                    child: new InkWell(
-                      onTap: () => print("I have tapped in quiz_page file"),
-                      child: new Center(
-                      child: new Container(
+              child: new Material(
+                color: Colors.greenAccent,
+                child: new InkWell(
+                  onTap: () => print("I have tapped true button"),
+                  child: new Center(
+                    child: new Container(
                       child: new Text("True"),
                     ),
                   ),
                 ),
               ),
             ),
-
             new Expanded(
                 child: new Material(
-                  color: Colors.greenAccent,
-                  child: new InkWell(
-                    onTap: () => print("I have tapped in quiz_page file"),
-                    child: new Center(
-                      child: new Container(
-                        child: new Text("True"),
-                      ),
-                    ),
+              color: Colors.redAccent,
+              child: new InkWell(
+                onTap: () => print("I have tapped false button"),
+                child: new Center(
+                  child: new Container(
+                    child: new Text("True"),
                   ),
-                )
-            )
-
+                ),
+              ),
+            ))
           ],
         )
       ],
     );
-
   }
 }
